d73cf709759905afda56ce40f484dbf8e7fa556e
Vixting <vixting@gmail.com>
2026-05-10 23:54:19 +0100

    fix

--- Content.Client/UserInterface/Systems/Chat/Widgets/ChatBox.xaml.cs
+++ Content.Client/UserInterface/Systems/Chat/Widgets/ChatBox.xaml.cs
@@ -354,7 +354,7 @@ public partial class ChatBox : UIWidget
         formatted.PushColor(color);
         // RMC14
         if (!string.IsNullOrWhiteSpace(languageIcon))
-            formatted.AddMarkupOrThrow($"[langicon path=\"{FormattedMessage.EscapeText(languageIcon)}\"][/langicon]");
+            formatted.AddMarkupOrThrow($"[langicon language=\"{FormattedMessage.EscapeText(languageIcon)}\"][/langicon]");
         // RMC14
         formatted.AddMarkupOrThrow(message);
         formatted.Pop();
