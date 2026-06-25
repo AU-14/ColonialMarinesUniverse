75f797276f30d98906304377532041c4a29f2cda
maertayn <maertayn@cyber-wizard.com>
2026-06-25 15:08:33 +0200

    🔧auThirdParty component is renamed to ThirdParty

---
+++ Content.CMU/Server/Ops/ThirdParty/SpawnThirdPartyToolComponent.cs
@@ -0,0 +1,13 @@
+using Content.Shared._CMU14.Threats;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._CMU14.Ops.ThirdParty;
+
+[RegisterComponent]
+public sealed partial class SpawnThirdPartyToolComponent : Component
+{
+    [DataField("dropship")]
+    public bool Dropship = true;
+    [DataField("party", required: true)]
+    public ProtoId<ThirdPartyPrototype> Party;
+}
