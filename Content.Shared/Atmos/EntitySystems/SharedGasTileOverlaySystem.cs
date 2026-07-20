d1d01970393926d6cbce12970f115c3c7a7a6562
TheHellFireo <ussfkamurof55@gmail.com>
2026-07-20 12:11:10 +0300

    merge: repair gas overlay state structure

--- Content.Shared/Atmos/EntitySystems/SharedGasTileOverlaySystem.cs
+++ Content.Shared/Atmos/EntitySystems/SharedGasTileOverlaySystem.cs
@@ -4,13 +4,71 @@ using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
 
-namespace Content.Shared.Atmos.EntitySystems
+namespace Content.Shared.Atmos.EntitySystems;
+
+public abstract partial class SharedGasTileOverlaySystem : EntitySystem
 {
-    public abstract partial class SharedGasTileOverlaySystem : EntitySystem
-    {
         public const byte ChunkSize = 8;
         protected float AccumulatedFrameTime;
         protected bool PvsEnabled;
+
+        [Dependency] protected IConfigurationManager ConfMan = default!;
+        [Dependency] private SharedAtmosphereSystem _atmosphere = default!;
+
+        /// <summary>
+        ///     array of the ids of all visible gases.
+        /// </summary>
+        public int[] VisibleGasId = default!;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            SubscribeLocalEvent<GasTileOverlayComponent, ComponentGetState>(OnGetState);
+
+            List<int> visibleGases = new();
+
+            for (var i = 0; i < Atmospherics.TotalNumberOfGases; i++)
+            {
+                var gasPrototype = _atmosphere.GetGas(i);
+                if (gasPrototype.GasOverlaySprite != null)
+                    visibleGases.Add(i);
+            }
+            VisibleGasId = visibleGases.ToArray();
+        }
+
+        private void OnGetState(EntityUid uid, GasTileOverlayComponent component, ref ComponentGetState args)
+        {
+            if (PvsEnabled && !args.ReplayState)
+                return;
+
+            // Should this be a full component state or a delta-state?
+            if (args.FromTick <= component.CreationTick || args.FromTick <= component.ForceTick)
+            {
+                args.State = new GasTileOverlayState(component.Chunks);
+                return;
+            }
+
+            var data = new Dictionary<Vector2i, GasOverlayChunk>();
+            foreach (var (index, chunk) in component.Chunks)
+            {
+                if (chunk.LastUpdate >= args.FromTick)
+                    data[index] = chunk;
+            }
+
+            args.State = new GasTileOverlayDeltaState(data, new(component.Chunks.Keys));
+        }
+
+        public static Vector2i GetGasChunkIndices(Vector2i indices)
+        {
+            return new Vector2i((int)MathF.Floor((float)indices.X / ChunkSize), (int)MathF.Floor((float)indices.Y / ChunkSize));
+        }
+
+        [Serializable, NetSerializable]
+        public readonly struct GasOverlayData : IEquatable<GasOverlayData>
+        {
+        [ViewVariables] public readonly byte FireState;
+        [ViewVariables] public readonly byte[] Opacity;
+        // TODO change fire color based on ByteTemp
 
         /// <summary>
         /// Network-synced air temperature, compressed to a single byte per tile for bandwidth optimization.
