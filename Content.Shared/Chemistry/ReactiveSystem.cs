1219c9dfac377d743395f146b5ace074dbaf6e2e
TheHellFireo <ussfkamurof55@gmail.com>
2026-07-20 13:05:47 +0300

    port: align reactive chemistry events

--- Content.Shared/Chemistry/ReactiveSystem.cs
+++ Content.Shared/Chemistry/ReactiveSystem.cs
@@ -1,5 +1,3 @@
-using Content.Shared._RMC14.Chemistry.Reagent;
-using Content.Shared.Administration.Logs;
 using Content.Shared.Chemistry.Components;
 using Content.Shared.Chemistry.Reagent;
 using Content.Shared.FixedPoint;
@@ -11,37 +9,47 @@ namespace Content.Shared.Chemistry;
 [UsedImplicitly]
 public sealed partial class ReactiveSystem : EntitySystem
 {
-    [Dependency] private IPrototypeManager _prototypeManager = default!;
-    [Dependency] private IRobustRandom _robustRandom = default!;
-    [Dependency] private ISharedAdminLogManager _adminLogger = default!;
-
     // TODO: Someone add documentation, I beg you
     public void DoEntityReaction(EntityUid uid, Solution solution, ReactionMethod method)
     {
         foreach (var reagent in solution.Contents.ToArray())
         {
-            ReactionEntity(uid, method, reagent);
+            ReactionEntity(uid, method, reagent, solution);
         }
     }
 
     public void ReactionEntity(EntityUid uid, ReactionMethod method, ReagentQuantity reagentQuantity)
     {
-        // We throw if the reagent specified doesn't exist.
-        var proto = _prototypeManager.IndexReagent<ReagentPrototype>(reagentQuantity.Reagent.Prototype);
-        ReactionEntity(uid, method, proto, reagentQuantity, source);
+        ReactionEntity(uid, method, reagentQuantity, null);
     }
 
-    public void ReactionEntity(EntityUid uid, ReactionMethod method, ReagentPrototype proto,
-        ReagentQuantity reagentQuantity, Solution? source)
+    public void ReactionEntity(
+        EntityUid uid,
+        ReactionMethod method,
+        ReagentQuantity reagentQuantity,
+        Solution? source)
     {
-        if (!TryComp(uid, out ReactiveComponent? reactive))
+        if (reagentQuantity.Quantity == FixedPoint2.Zero)
             return;
 
         // We throw if the reagent specified doesn't exist.
         if (!ProtoMan.Resolve<ReagentPrototype>(reagentQuantity.Reagent.Prototype, out var proto))
             return;
 
-        var ev = new ReactionEntityEvent(method, reagentQuantity, proto);
+        ReactionEntity(uid, method, proto, reagentQuantity, source);
+    }
+
+    public void ReactionEntity(
+        EntityUid uid,
+        ReactionMethod method,
+        ReagentPrototype proto,
+        ReagentQuantity reagentQuantity,
+        Solution? source)
+    {
+        if (reagentQuantity.Quantity == FixedPoint2.Zero)
+            return;
+
+        var ev = new ReactionEntityEvent(method, reagentQuantity, proto, source);
         RaiseLocalEvent(uid, ref ev);
     }
 }
@@ -53,4 +61,8 @@ Ingestion,
 }
 
 [ByRefEvent]
-public readonly record struct ReactionEntityEvent(ReactionMethod Method, ReagentQuantity ReagentQuantity, ReagentPrototype Reagent);
+public readonly record struct ReactionEntityEvent(
+    ReactionMethod Method,
+    ReagentQuantity ReagentQuantity,
+    ReagentPrototype Reagent,
+    Solution? Source = null);
