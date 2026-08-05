192adf70f744f740e81fecdc2c0b920299eaa09d
TheHellFireo <ussfkamurof55@gmail.com>
2026-08-05 03:21:06 +0300

    Fix CMU hands and vocal emote sounds

--- Content.IntegrationTests/_CMU14/Medical/CmuHumanVisualBodyTest.cs
+++ Content.IntegrationTests/_CMU14/Medical/CmuHumanVisualBodyTest.cs
@@ -3,7 +3,10 @@ using Content.IntegrationTests.Fixtures;
 using Content.IntegrationTests.Fixtures.Attributes;
 using Content.Shared.Body;
 using Content.Shared.Body.Part;
+using Content.Shared.Hands.Components;
+using Content.Shared.Hands.EntitySystems;
 using Content.Shared.Humanoid;
+using Content.Shared.Movement.Pulling.Systems;
 using Robust.Shared.Enums;
 using Robust.Shared.GameObjects;
 
@@ -30,6 +33,29 @@ public sealed class CmuHumanVisualBodyTest : GameTest
             var parts = SEntMan.EntityQuery<BodyPartComponent>()
                 .Count(part => part.Body == human);
             Assert.That(parts, Is.EqualTo(10), "CMMobHuman did not build its complete CMU body-part graph.");
+        });
+    }
+
+    [Test]
+    public async Task HumanSpawnsWithUsableHands()
+    {
+        var map = await Pair.CreateTestMap();
+
+        await Server.WaitAssertion(() =>
+        {
+            var human = SEntMan.SpawnEntity("CMMobHuman", map.GridCoords);
+            var pullTarget = SEntMan.SpawnEntity("CMMobHuman", map.GridCoords);
+            var item = SEntMan.SpawnEntity("RMCWeaponRifleM54C", map.GridCoords);
+            var hands = SEntMan.System<SharedHandsSystem>();
+            var pulling = SEntMan.System<PullingSystem>();
+            var handsComponent = SEntMan.GetComponent<HandsComponent>(human);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(handsComponent.Count, Is.EqualTo(2));
+                Assert.That(hands.TryPickupAnyHand(human, item), Is.True);
+                Assert.That(pulling.TryStartPull(human, pullTarget), Is.True);
+            });
         });
     }
 }
