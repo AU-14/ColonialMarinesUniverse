45c30a0f09a799d9af6362762d5a1791a2fa50bb
TheHellFireo <ussfkamurof55@gmail.com>
2026-08-11 19:35:59 +0300

    Fix autoinjector solution warnings

--- Content.IntegrationTests/_RMC14/Medical/AutoinjectorSolutionTest.cs
+++ Content.IntegrationTests/_RMC14/Medical/AutoinjectorSolutionTest.cs
@@ -16,6 +16,12 @@ public sealed class AutoinjectorSolutionTest
     [TestCase("CMInaprovalineAutoInjector")]
     [TestCase("CMKelotaneAutoInjector")]
     [TestCase("CMTricordrazineAutoInjector")]
+    [TestCase("RMCIronAutoInjector")]
+    [TestCase("AU14NaloxoneAutoInjector")]
+    [TestCase("AU14FirstAidAutoInjectorNoSkill")]
+    [TestCase("AU14OxycodoneAutoInjector")]
+    [TestCase("AU14ParacetamolAutoInjector")]
+    [TestCase("AU14TramadolAutoInjector")]
     public async Task MedicalVendorAutoinjectorsSpawnFilled(string prototype)
     {
         await using var pair = await PoolManager.GetServerClient();
@@ -32,6 +38,7 @@ public sealed class AutoinjectorSolutionTest
                 Assert.That(entMan.TryGetComponent<SolutionComponent>(injector, out var injectorSolution), Is.True);
                 Assert.That(injectorSolution.Id, Is.EqualTo("pen"));
                 Assert.That(injectorSolution.Solution.Volume, Is.GreaterThan(FixedPoint2.Zero));
+                Assert.That(entMan.HasComponent<SolutionContainerManagerComponent>(injector), Is.False);
                 Assert.That(entMan.GetComponent<ExaminableSolutionComponent>(injector).Solution, Is.EqualTo("pen"));
                 Assert.That(entMan.GetComponent<SolutionContainerVisualsComponent>(injector).SolutionName, Is.EqualTo("pen"));
                 Assert.That(solution.TryGetSolution(injector, "pen", out _, out var contents), Is.True);
