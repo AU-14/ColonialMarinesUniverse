973c027cc1fa7315d816ea544bedf46006d34352
maertayn <maertayn@cyber-wizard.com>
2026-08-20 20:18:57 +0200

    🩹 fix waitForRot carrier and stomp collision group using

--- Content.Shared/Kitchen/ButcherableComponent.RMC.cs
+++ Content.Shared/Kitchen/ButcherableComponent.RMC.cs
@@ -9,6 +9,9 @@ public sealed partial class ButcherableComponent
     /// </summary>
     [DataField("butcheringType"), AutoNetworkedField]
     public ButcheringType Type = ButcheringType.Knife;
+
+    [DataField("waitForRot"), AutoNetworkedField] // CMU14: gate butchering of unrevivable corpses until they rot
+    public bool WaitForRot = false;
 }
 
 public enum ButcheringType : byte
