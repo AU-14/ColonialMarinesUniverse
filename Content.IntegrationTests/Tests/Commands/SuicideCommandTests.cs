e37d50c40ef6d072c4cff686ad76e315c625e7ec
maertayn <maertayn@cyber-wizard.com>
2026-08-21 07:06:34 +0200

    🩹 restore suicide test ignore dropped by test port

--- Content.IntegrationTests/Tests/Commands/SuicideCommandTests.cs
+++ Content.IntegrationTests/Tests/Commands/SuicideCommandTests.cs
@@ -22,6 +22,7 @@ using Robust.Shared.Prototypes;
 namespace Content.IntegrationTests.Tests.Commands;
 
 [TestFixture]
+[Ignore("RMC14 disables suicides")]
 public sealed class SuicideCommandTests : GameTest
 {
 
