ef9003b30aa702fac27f6bd36821ce2b6f0a5862
deepdarkdepths <155149356+deepdarkdepths@users.noreply.github.com>
2026-09-22 00:05:02 -0400

    networked

--- Content.Shared/_RMC14/Xenonids/Despoiler/XenoDespoilerLingeringAcidComponent.cs
+++ Content.Shared/_RMC14/Xenonids/Despoiler/XenoDespoilerLingeringAcidComponent.cs
@@ -1,6 +1,7 @@
+using Robust.Shared.GameStates;
 namespace Content.Shared._RMC14.Xenonids.Despoiler;
 
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class XenoDespoilerLingeringAcidComponent : Component
 {
     [DataField]
