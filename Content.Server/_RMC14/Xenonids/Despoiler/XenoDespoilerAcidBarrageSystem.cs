6f1a2d1317da931ab74018ac4d2bc13dcc45f98e
deepdarkdepths <155149356+deepdarkdepths@users.noreply.github.com>
2026-09-21 23:44:53 -0400

    revert

--- Content.Server/_RMC14/Xenonids/Despoiler/XenoDespoilerAcidBarrageSystem.cs
+++ Content.Server/_RMC14/Xenonids/Despoiler/XenoDespoilerAcidBarrageSystem.cs
@@ -211,19 +211,8 @@ public sealed partial class XenoDespoilerAcidBarrageSystem : EntitySystem
 
         for (var i = 0; i < count; i++)
         {
-            float spread;
+            var angle = baseAngle + ((float)_random.NextDouble() * 2f - 1f) * scatterRad; // CMU14
 
-            if (count == 1)
-                spread = 0f;
-            else
-            {
-                spread = MathHelper.Lerp(
-                    -scatterRad,
-                    scatterRad,
-                    (float)i / (count - 1));
-            }
-
-            var angle = baseAngle + spread;
             var unit = new Vector2(MathF.Cos(angle), MathF.Sin(angle));
             var rangeTiles = _random.Next(action.MinRangeTiles, action.MaxRangeTiles + 1);
 
--- Content.Shared/_RMC14/Xenonids/Despoiler/XenoDespoilerHypertensionSystem.cs
+++ Content.Shared/_RMC14/Xenonids/Despoiler/XenoDespoilerHypertensionSystem.cs
@@ -1,32 +1,10 @@
 using Robust.Shared.Timing;
-using Content.Shared._RMC14.Armor;
 
 namespace Content.Shared._RMC14.Xenonids.Despoiler;
 
 public sealed partial class XenoDespoilerHypertensionSystem : EntitySystem
 {
     [Dependency] private IGameTiming _timing = default!;
-    [Dependency] private CMArmorSystem _armor = default!;
-
-    public override void Initialize()
-    {
-        SubscribeLocalEvent<
-            XenoDespoilerHypertensionComponent,
-            CMGetArmorEvent>(OnGetArmor);
-    }
-
-    private void OnGetArmor(
-        Entity<XenoDespoilerHypertensionComponent> ent,
-        ref CMGetArmorEvent args)
-    {
-        args.XenoArmor += (ent.Comp.Stacks / 2) * 5;
-    }
-
-    private void RefreshArmor(EntityUid uid)
-    {
-        if (TryComp<CMArmorComponent>(uid, out var armor))
-            _armor.UpdateArmorValue((uid, armor));
-    }
 
     public void AddSlashPoints(EntityUid uid, XenoDespoilerHypertensionComponent comp)
     {
@@ -41,8 +19,6 @@ public sealed partial class XenoDespoilerHypertensionSystem : EntitySystem
         comp.Points += amount;
         comp.LastActivityAt = _timing.CurTime;
 
-        var oldStacks = comp.Stacks;
-
         while (comp.Points >= comp.PointsPerStack && comp.Stacks < comp.MaxStacks)
         {
             comp.Points -= comp.PointsPerStack;
@@ -51,9 +27,6 @@ public sealed partial class XenoDespoilerHypertensionSystem : EntitySystem
 
         if (comp.Stacks >= comp.MaxStacks)
             comp.Points = 0;
-
-        if (oldStacks != comp.MaxStacks)
-            RefreshArmor(uid);
 
         Dirty(uid, comp);
     }
@@ -65,30 +38,8 @@ public sealed partial class XenoDespoilerHypertensionSystem : EntitySystem
 
         comp.Stacks -= count;
 
-        RefreshArmor(uid);
-
         Dirty(uid, comp);
         return true;
-    }
-
-    public void RemoveStacks(
-        EntityUid uid,
-        XenoDespoilerHypertensionComponent comp,
-        int amount)
-    {
-        if (amount <= 0)
-            return;
-
-        var oldStacks = comp.Stacks;
-
-        comp.Stacks = Math.Max(0, comp.Stacks - amount);
-
-        if (oldStacks == comp.Stacks)
-            return;
-
-        RefreshArmor(uid);
-
-        Dirty(uid, comp);
     }
 
     public override void Update(float frameTime)
@@ -105,16 +56,14 @@ public sealed partial class XenoDespoilerHypertensionSystem : EntitySystem
 
             comp.Points -= comp.DecayPerSecond * frameTime;
 
-            var oldStacks = comp.Stacks;
-
             while (comp.Points < 0 && comp.Stacks > 0)
             {
                 comp.Stacks--;
                 comp.Points += comp.PointsPerStack;
             }
 
-            if (oldStacks != comp.Stacks)
-                RefreshArmor(uid);
+            if (comp.Stacks <= 0 && comp.Points < 0)
+                comp.Points = 0;
 
             Dirty(uid, comp);
         }
